7ff564cd25ac6c4151beda326720a50c4ef4daa4
Kuba <thekubajacob@gmail.com>
2023-11-02 20:54:03 +0100

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
+import { Sidebar } from '..'
 import useStyles from './styles';
 
 const NavBar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme();
   const isAuthenticated = true;
 
   return (
+    <>
     <AppBar position="fixed">
       <Toolbar className={classes.toolbar}>
         { isMobile && (
@@ -19,7 +22,7 @@ const NavBar = () => {
           color="inherit"
           edge="start"
           style={{ outline: 'none' }}
-          onClick={() => {}}
+          onClick={()=> setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
           className={classes.menuButton}
         >
           <Menu />
@@ -51,6 +54,28 @@ const NavBar = () => {
         {isMobile && 'Search...'}
       </Toolbar>
     </AppBar>
+    <div>
+        <nav className={classes.drawer}>
+            {isMobile ? (
+              <Drawer
+                variant="temporary"
+                anchor="right"
+                open={mobileOpen}
+                onClose={()=> setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+                className={classes.drawerBackground}
+                classes={{paper: classes.drawerPaper}}
+                ModalProps={{ keepMounted: true }}
+              >
+                <Sidebar setMobileOpen={setMobileOpen} />
+              </Drawer>
+            ) : (
+              <Drawer classes={{ paper:classes.drawerPaper }} variant="permanent" open>  
+                <Sidebar setMobileOpen={setMobileOpen} />
+              </Drawer>
+            )}
+        </nav>
+    </div>
+    </>
   );
 };
 
