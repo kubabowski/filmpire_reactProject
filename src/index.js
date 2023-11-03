6050765693a8b1ebb2f5d80b66862846227238ee
Kuba <thekubajacob@gmail.com>
2023-11-03 23:46:35 +0100

    fetch

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const theme = createTheme({});
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
 
