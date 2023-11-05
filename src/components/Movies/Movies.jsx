c6e3dcb56d8c0939349525ed4c8d19b82603fb0f
Kuba <thekubajacob@gmail.com>
2023-11-06 00:18:37 +0100

    movie

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,14 +3,14 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import MovieList from '../MovieList/MovieList';
 
 const Movies = () => {
   const { data } = useGetMoviesQuery();
-  console.log(data);
 
   return (
     <div>
-      Moviess
+      <MovieList movies={data} />
     </div>
   );
 };
