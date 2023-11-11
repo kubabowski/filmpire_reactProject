3d345bd0530f99768469b94c9812cddfd27be10e
Kuba <thekubajacob@gmail.com>
2023-11-11 23:18:13 +0100

    search

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,25 +7,7 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
-    // //* Get Movies by [Type]
-    // getMovies: builder.query({
-    //   query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
-    // }),
-    // //* Get Genres
-    // getGenres: builder.query({
-    //   query: ({ genreIdOrCategoryName, page }) => {
-    //     //* Get Movies by Category
-    //     if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
-    //       return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
-    //     }
-    //     //* Get Movies by Genre
-    //     if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
-    //       return `discover/movie?with_genres=${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
-    //     }
-    //     //* Get Popular Movies
-    //     return `genre/movie/list?api_key=${tmdbApiKey}`;
-    //   },
-    // }),
+
     //* Get Genres
     getGenres: builder.query({
       query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
@@ -42,6 +24,8 @@ export const tmdbApi = createApi({
           console.log('tutaj');
           return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
         }
+
+        //* Search Movie */
         //* Get Popular Movies
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
