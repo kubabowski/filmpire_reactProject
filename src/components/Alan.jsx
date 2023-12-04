01da5eb7f83e6807e0e49eb97a26be8575b80c0f
Kuba <thekubajacob@gmail.com>
2023-12-05 00:29:08 +0100

    alan ai

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,17 +1,35 @@
 import React, { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { ColorModeContext } from '../utils/ToggleColorMode';
 import { fetchToken } from '../utils';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const history = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     alanBtn({
       key: '8b905f664cc9ff50f285c64d0a24aeeb2e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, mode }) => {
-        if (command === 'changeMode') {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+          if (foundGenre) {
+            history('/');
+            // window.location.href = '/';
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top rated' : genreOrCategory;
+            history('/');
+            // window.location.href = '/';
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
           if (mode === 'light') {
             setMode('light');
           } else {
@@ -21,7 +39,10 @@ const useAlan = () => {
           fetchToken();
         } else if (command === 'logout') {
           localStorage.clear();
-          window.location.href = '/';
+          history.push('/');
+        //   window.location.href = '/';
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
         }
       },
     });
